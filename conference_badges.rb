7fdf55d02daa5df067c02ed38615c5f76e794a1f
Amanda Ackerman <ackerm44@gmail.com>
2017-10-23 21:08:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array << "Hello, my name is #{name}."
+  end
+  new_array
+end
+
+def assign_rooms(array)
+  new_array = []
+  array.each do |name|
+    room = array.index(name) + 1
+    new_array << "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+  new_array
+end
+
+def printer(array)
+  array = batch_badge_creator(array)
+  array.each do |badge|
+      puts badge
+    end
+  array = assign_rooms(array)
+  array.each do |badge|
+      puts badge
+end
